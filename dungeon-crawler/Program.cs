627b0fab8cd0cc133bbe9a8a4411294f724ebe6e
Campbell Brown <campbellbrown093@gmail.com>
2021-10-22 17:56:05 +1300

    Created VS project

---
+++ dungeon-crawler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dungeon_crawler
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
