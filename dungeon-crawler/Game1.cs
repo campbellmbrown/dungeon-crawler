45600b89241ed9a9b7db78fb4b03392d2e581f2d
Campbell Brown <campbellbrown093@gmail.com>
2021-10-30 12:57:31 +1300

    4: Bumped version

--- dungeon-crawler/Game1.cs
+++ dungeon-crawler/Game1.cs
@@ -27,7 +27,7 @@ namespace dungeoncrawler
         private GameState _gameState;
         private PlayingState _playingState;
 
-        public const string VERSION_STR = "v0.1.2";
+        public const string VERSION_STR = "v0.1.3";
 
         public Game1()
         {
