4008c3e11a003f28c12daa1b41fe031597c03b64
Campbell Brown <campbellbrown093@gmail.com>
2021-12-20 13:50:46 +1300

    27: Formatting

--- scripts/map_generation.py
+++ scripts/map_generation.py
@@ -1,11 +1,12 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from enum import Enum
 import logging
-import colorlog
-from matplotlib.widgets import Button
-import time
 import statistics
+from enum import Enum
+
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+import numpy as np
+import colorlog
+
 
 def logger_config(name=None):
     """Setup the logging environment."""
@@ -14,20 +15,22 @@ def logger_config(name=None):
     else:
         log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
-    format_str = '%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s'
-    date_format = '%Y-%m-%d %H:%M:%S'
-    cformat = '%(log_color)s' + format_str
-    colors = {'DEBUG': 'green',
-              'INFO': 'white',
-              'WARNING': 'bold_yellow',
-              'ERROR': 'bold_red',
-              'CRITICAL': 'bold_purple'}
-    formatter = colorlog.ColoredFormatter(cformat, date_format,
-                                          log_colors=colors)
+    format_str = "%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    cformat = "%(log_color)s" + format_str
+    colors = {
+        "DEBUG": "green",
+        "INFO": "white",
+        "WARNING": "bold_yellow",
+        "ERROR": "bold_red",
+        "CRITICAL": "bold_purple",
+    }
+    formatter = colorlog.ColoredFormatter(cformat, date_format, log_colors=colors)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
     log.addHandler(stream_handler)
-    return log 
+    return log
+
 
 STARTING_X = 0
 STARTING_Y = 0
@@ -47,62 +50,68 @@ BRANCH_AT_DIRECTION_CHANGE_CHANCE = 50
 ROOM_AT_DIRECTION_CHANGE_CHANCE = 10
 SHIFT_CHANCE = 20
 
+
 class Direction(Enum):
-    Up = 0
-    Down = 1
-    Left = 2
-    Right = 3
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
 
 index_deltas = {
-    Direction.Up: (0, -1),
-    Direction.Down: (0, 1),
-    Direction.Left: (-1, 0),
-    Direction.Right: (1, 0),
+    Direction.UP: (0, -1),
+    Direction.DOWN: (0, 1),
+    Direction.LEFT: (-1, 0),
+    Direction.RIGHT: (1, 0),
 }
 
 DEFAULT_DIRECTION_WEIGHT = 1
 PRIORITY_DIRECTION_WEIGHT = 3
 opposite_direction = {
-    Direction.Up: Direction.Down,
-    Direction.Down: Direction.Up,
-    Direction.Left: Direction.Right,
-    Direction.Right: Direction.Left,
+    Direction.UP: Direction.DOWN,
+    Direction.DOWN: Direction.UP,
+    Direction.LEFT: Direction.RIGHT,
+    Direction.RIGHT: Direction.LEFT,
 }
 change_direction_weights = {
-    Direction.Up: DEFAULT_DIRECTION_WEIGHT,
-    Direction.Down: DEFAULT_DIRECTION_WEIGHT,
-    Direction.Left: DEFAULT_DIRECTION_WEIGHT,
-    Direction.Right: DEFAULT_DIRECTION_WEIGHT,
+    Direction.UP: DEFAULT_DIRECTION_WEIGHT,
+    Direction.DOWN: DEFAULT_DIRECTION_WEIGHT,
+    Direction.LEFT: DEFAULT_DIRECTION_WEIGHT,
+    Direction.RIGHT: DEFAULT_DIRECTION_WEIGHT,
 }
 branch_direction_weights = {
-    Direction.Up: DEFAULT_DIRECTION_WEIGHT,
-    Direction.Down: DEFAULT_DIRECTION_WEIGHT,
-    Direction.Left: DEFAULT_DIRECTION_WEIGHT,
-    Direction.Right: DEFAULT_DIRECTION_WEIGHT,
+    Direction.UP: DEFAULT_DIRECTION_WEIGHT,
+    Direction.DOWN: DEFAULT_DIRECTION_WEIGHT,
+    Direction.LEFT: DEFAULT_DIRECTION_WEIGHT,
+    Direction.RIGHT: DEFAULT_DIRECTION_WEIGHT,
 }
 turn_90_deg_options = {
-    Direction.Up: [Direction.Left, Direction.Right], 
-    Direction.Down: [Direction.Left, Direction.Right], 
-    Direction.Left: [Direction.Up, Direction.Down], 
-    Direction.Right: [Direction.Up, Direction.Down], 
+    Direction.UP: [Direction.LEFT, Direction.RIGHT],
+    Direction.DOWN: [Direction.LEFT, Direction.RIGHT],
+    Direction.LEFT: [Direction.UP, Direction.DOWN],
+    Direction.RIGHT: [Direction.UP, Direction.DOWN],
 }
 
+
 class GridSquare:
-    def __init__(self, x: int, y:int):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
 
 def percent_chance(percent: int) -> bool:
     return np.random.randint(100) < percent
 
+
 def gaussian(mean: int, std_dev: int, low_limit: int = -999, up_limit: int = 999) -> int:
-        val = 0
-        for _ in range(12):
-            val += np.random.random()
-        val -= 6
-        val = mean + (val * std_dev)
-        ret = int(np.round(val))
-        return np.clip(ret, low_limit, up_limit)
+    val = 0
+    for _ in range(12):
+        val += np.random.random()
+    val -= 6
+    val = mean + (val * std_dev)
+    ret = int(np.round(val))
+    return np.clip(ret, low_limit, up_limit)
+
 
 def choose_weighted(my_list, weights: dict):
     total_weights = 0
@@ -122,22 +131,23 @@ def choose_weighted(my_list, weights: dict):
 class MapGenerator:
     def __init__(self):
         self.clean()
+        self.distance_until_direction_change = 0
 
     def clean(self):
         self.grid_squares = []
-        self.seed = np.random.randint(0, 2**31)
+        self.seed = np.random.randint(0, 2 ** 31)
         np.random.seed(self.seed)
 
     def plot(self):
         fig, ax = plt.subplots()
-        
+
         def prepare_map() -> np.ndarray:
             max_x = self._get_max_x()
             min_x = self._get_min_x()
             max_y = self._get_max_y()
             min_y = self._get_min_y()
             logger.debug(f"max X: {max_x} min X: {min_x} max Y: {max_y} min Y: {min_y}")
-            
+
             EDGE_BUFFER = 2
             width = (max_x - min_x) + 1 + (2 * EDGE_BUFFER)
             height = (max_y - min_y) + 1 + (2 * EDGE_BUFFER)
@@ -149,18 +159,22 @@ class MapGenerator:
             elif height > width:
                 extra_in_width = (height - width) // 2
             grid_size = max(width, height)
-            
+
             zvals = np.zeros((grid_size, grid_size))
             for grid_square in self.grid_squares:
-                zvals[grid_square.y - min_y + EDGE_BUFFER + extra_in_height][grid_square.x - min_x + EDGE_BUFFER + extra_in_width] = 1
-            zvals[STARTING_Y - min_y + EDGE_BUFFER + extra_in_height][STARTING_X - min_x + EDGE_BUFFER + extra_in_width] = 2
+                x_idx = grid_square.y - min_y + EDGE_BUFFER + extra_in_height
+                y_idx = grid_square.x - min_x + EDGE_BUFFER + extra_in_width
+                zvals[x_idx][y_idx] = 1
+            x_idx = STARTING_Y - min_y + EDGE_BUFFER + extra_in_height
+            y_idx = STARTING_X - min_x + EDGE_BUFFER + extra_in_width
+            zvals[x_idx][y_idx] = 2
             return zvals
 
         def update_title():
             ax.set_title(f"Seed {self.seed} (floor area = {len(self.grid_squares)})")
 
-        
         def new_map(event):
+            logger.debug(f"Event {event}")
             logger.info("Generating new map")
             self.clean()
             self.generate()
@@ -169,12 +183,11 @@ class MapGenerator:
             im.set_data(zvals)
             fig.canvas.draw_idle()
 
-
         zvals = prepare_map()
         im = ax.imshow(zvals)
         update_title()
-        BUTTON_HEIGHT = 0.1
-        axnew = plt.axes([0.85, 0.45, 0.1, 0.1])
+        BUTTON_SIZE = 0.1
+        axnew = plt.axes([0.85, 0.45, BUTTON_SIZE, BUTTON_SIZE])
         bnew = Button(axnew, "New")
         bnew.on_clicked(new_map)
 
@@ -191,7 +204,7 @@ class MapGenerator:
         self.distance_until_direction_change = self._new_distance_until_direction_change()
         logger.info(f"Distance until direction change is {self.distance_until_direction_change}")
 
-        for idx in range(main_path_length):
+        for _ in range(main_path_length):
             if self.distance_until_direction_change == 0:
                 current_direction = self._change_direction(current_grid_square, current_direction)
 
@@ -214,10 +227,13 @@ class MapGenerator:
             if progress >= next_print_at_percent:
                 next_print_at_percent += PRINT_EVERY_PERCENT
                 print(f"Statistics: {progress:.2f}% complete")
-        fig, (ax1) = plt.subplots(1, sharex=True)
-        print(f"Mean: {statistics.mean(floor_areas)}, mode: {statistics.mode(floor_areas)}, median: {statistics.median(floor_areas)}, standard deviation: {statistics.stdev(floor_areas)}")
+        _, (ax1) = plt.subplots(1, sharex=True)
+        print(f"Mean: {statistics.mean(floor_areas)}")
+        print(f"mode: {statistics.mode(floor_areas)}")
+        print(f"median: {statistics.median(floor_areas)}")
+        print(f"standard deviation: {statistics.stdev(floor_areas)}")
         ax1.grid(zorder=0)
-        ax1.hist(floor_areas, color = 'blue', edgecolor = 'black', bins=(max(floor_areas) - min(floor_areas)), zorder=3)
+        ax1.hist(floor_areas, color="blue", edgecolor="black", bins=(max(floor_areas) - min(floor_areas)), zorder=3)
         ax1.set_title(f"Level Generation Floor Area ({len(floor_areas)} Generations)")
         ax1.set_ylabel("Number of Occurrences")
         ax1.set_xlabel("Floor Area")
@@ -231,10 +247,7 @@ class MapGenerator:
         self.distance_until_direction_change = self._new_distance_until_direction_change()
         logger.info(f"New distance until direction change is {self.distance_until_direction_change}")
         if percent_chance(BRANCH_AT_DIRECTION_CHANGE_CHANCE):
-            options = [
-                previous_direction,
-                opposite_direction[new_direction]
-            ]
+            options = [previous_direction, opposite_direction[new_direction]]
             self._create_branch(current_gridsquare, choose_weighted(options, branch_direction_weights))
         if percent_chance(ROOM_AT_DIRECTION_CHANGE_CHANCE):
             self._create_room(current_gridsquare)
@@ -244,8 +257,10 @@ class MapGenerator:
         logger.info(f"Creating branch at [{start.x}, {start.y}] in direction {branch_direction}")
         branch_current_gridsquare = start
         branch_length = np.random.randint(BRANCH_MIN_LENGTH, BRANCH_MAX_LENGTH + 1)
-        for idx in range(branch_length):
-            branch_current_gridsquare = self._create_gridsquare_in_direction(branch_current_gridsquare, branch_direction)
+        for _ in range(branch_length):
+            branch_current_gridsquare = self._create_gridsquare_in_direction(
+                branch_current_gridsquare, branch_direction
+            )
         self._create_room(branch_current_gridsquare)
 
     def _new_distance_until_direction_change(self) -> int:
@@ -254,10 +269,17 @@ class MapGenerator:
     def _choose_new_direction(self, current_direction: Direction) -> Direction:
         return choose_weighted(turn_90_deg_options[current_direction], change_direction_weights)
 
-    def _create_room(self, center: GridSquare, minW: int = MIN_ROOM_WIDTH, maxW: int = MAX_ROOM_WIDTH, minH: int = MIN_ROOM_HEIGHT, maxH: int = MAX_ROOM_HEIGHT):
+    def _create_room(
+        self,
+        center: GridSquare,
+        min_w: int = MIN_ROOM_WIDTH,
+        max_w: int = MAX_ROOM_WIDTH,
+        min_h: int = MIN_ROOM_HEIGHT,
+        max_h: int = MAX_ROOM_HEIGHT,
+    ):
         logger.info(f"Creating room at [{center.x}, {center.y}]")
-        room_width = np.random.randint(minW, maxW + 1)
-        room_height = np.random.randint(minH, maxH + 1)
+        room_width = np.random.randint(min_w, max_w + 1)
+        room_height = np.random.randint(min_h, max_h + 1)
 
         logger.debug(f"The room width is {room_width}")
         logger.debug(f"The room height is {room_height}")
@@ -266,7 +288,7 @@ class MapGenerator:
 
         for x_idx in range(room_width):
             for y_idx in range(room_height):
-                corner = (x_idx == 0 or x_idx == room_width - 1) and (y_idx == 0 or y_idx == room_height - 1) 
+                corner = (x_idx == 0 or x_idx == room_width - 1) and (y_idx == 0 or y_idx == room_height - 1)
                 if not (corner and percent_chance(REMOVE_ROOM_CORNER_CHANCE)):
                     self._try_create_new_grid_square(origin_x + x_idx, origin_y + y_idx)
                 else:
@@ -286,7 +308,9 @@ class MapGenerator:
         logger.debug(f"branch_direction_weights are now {branch_direction_weights}")
 
     def _shift_sideways_one(self, current_grid_square: GridSquare, current_direction: Direction) -> GridSquare:
-            return self._create_gridsquare_in_direction(current_grid_square, np.random.choice(turn_90_deg_options[current_direction]))
+        return self._create_gridsquare_in_direction(
+            current_grid_square, np.random.choice(turn_90_deg_options[current_direction])
+        )
 
     def _create_gridsquare_in_direction(self, current: GridSquare, direction: Direction) -> GridSquare:
         idx_delta = index_deltas[direction]
@@ -304,7 +328,6 @@ class MapGenerator:
         else:
             logger.debug(f"Grid square at [{x_idx}, {y_idx}] already exists")
             return grid_square_exists
-
 
     def _get_max_x(self) -> int:
         return max(grid_square.x for grid_square in self.grid_squares)
