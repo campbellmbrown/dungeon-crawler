f8d64c0dd89094eab2636dd413cb920fad9bfc5d
Campbell Brown <campbellbrown093@gmail.com>
2022-01-21 17:34:36 +1300

    13: bitmasking and drawing walls

--- dungeon-crawler/GameStates/PlayingState/Tiles/Wall.cs
+++ dungeon-crawler/GameStates/PlayingState/Tiles/Wall.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using dungeoncrawler.Visual;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using System;
@@ -9,14 +10,44 @@ namespace dungeoncrawler.GameStates.PlayingState
 {
     public class Wall : GridSquare
     {
+        public const int WALL_HEIGHT = 23;
+        private readonly SpriteSheet _sprite;
+
+        public static Dictionary<int, Rectangle> textureRectangleLookup = new Dictionary<int, Rectangle>()
+        {
+            { 0, new Rectangle(0 * GRID_SQUARE_SIZE, 0 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 1, new Rectangle(1 * GRID_SQUARE_SIZE, 0 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 2, new Rectangle(2 * GRID_SQUARE_SIZE, 0 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 3, new Rectangle(3 * GRID_SQUARE_SIZE, 0 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 4, new Rectangle(0 * GRID_SQUARE_SIZE, 1 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 5, new Rectangle(1 * GRID_SQUARE_SIZE, 1 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 6, new Rectangle(2 * GRID_SQUARE_SIZE, 1 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 7, new Rectangle(3 * GRID_SQUARE_SIZE, 1 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 8, new Rectangle(0 * GRID_SQUARE_SIZE, 2 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 9, new Rectangle(1 * GRID_SQUARE_SIZE, 2 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 10, new Rectangle(2 * GRID_SQUARE_SIZE, 2 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 11, new Rectangle(3 * GRID_SQUARE_SIZE, 2 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 12, new Rectangle(0 * GRID_SQUARE_SIZE, 3 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 13, new Rectangle(1 * GRID_SQUARE_SIZE, 3 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 14, new Rectangle(2 * GRID_SQUARE_SIZE, 3 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) },
+            { 15, new Rectangle(3 * GRID_SQUARE_SIZE, 3 * WALL_HEIGHT, GRID_SQUARE_SIZE, WALL_HEIGHT) }
+        };
+
+        public void UpdateID(int id)
+        {
+            _sprite.ChangeTextureRectangle(id);
+        }
+
         public Wall(GridManager gridManager, int xIdx, int yIdx) :
             base(gridManager, xIdx, yIdx)
         {
+            _sprite = new SpriteSheet(Game1.textures["level_gray_brick"], textureRectangleLookup, 0);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawRectangle(new RectangleF(position.X, position.Y, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE), Color.Green * opacity);
+            Vector2 offset = new Vector2(0, GRID_SQUARE_SIZE - WALL_HEIGHT);
+            _sprite.Draw(spriteBatch, position + offset, opacity);
             base.Draw(spriteBatch);
         }
     }
